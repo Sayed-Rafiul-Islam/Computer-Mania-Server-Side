218f1831e0b6b03e55b2fddd83e0bab56d53d9f2
Yasin-Rafid <96468660+Yasin-Rafid@users.noreply.github.com>
2022-05-22 14:30:36 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+// =-------------------------- from my DB
+
+// user : computer-menia
+// pass : ddb5yunjxkafYLSZ
+
+const uri = "mongodb+srv://computer-menia:ddb5yunjxkafYLSZ@cluster0.m7qfs.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// --------------------------------------------
+
+async function run() {
+    try {
+        await client.connect();
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('running ')
+})
+app.listen(port, () => {
+    console.log('crud is running')
+})
